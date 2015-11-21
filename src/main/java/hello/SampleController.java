d67d068316777ed7b05b323c7fa6212380d90593
wzorce <wilk.michal1993@gmail.com>
2015-11-21 16:07:19 +0100

    logging. needs refactoring

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,22 +1,82 @@
 package hello;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 import products.Product;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    @Bean(name = "dataSource")
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://sql4.freesqldatabase.com:3306/sql497139");
+        driverManagerDataSource.setUsername("sql497139");//
+        driverManagerDataSource.setPassword("7lwadeYgm6");
+        return driverManagerDataSource;
     }
+
+    @ResponseBody  //TU DALEM RESPONSEBODY BO BYLO CIRCULAR VIEW PATH [user] na /user
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @ResponseBody
+    @RequestMapping("/getResources/")
+    public Map<String, Object> home() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("content", "Hello World");
+        model.put("current",name);
+        return model;
+    }
+
+//    @RequestMapping("/")
+//    @ResponseBody
+//    String home2() {
+//        return "Hello World!";
+//    }
 
 
     @RequestMapping("/getIndex/")
@@ -56,5 +116,82 @@ public class SampleController {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
+    }
+
+
+
+    @RequestMapping(value = "/addNewAccount", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void receiveNewConfiguration(@RequestBody Configuration configuration)
+    {
+
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Autowired //javax.sql.dataSource
+                DataSource dataSource;
+
+        @Autowired
+        public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+
+            auth.jdbcAuthentication().dataSource(dataSource)
+                    .usersByUsernameQuery(
+                            "select username,password, enabled from users where username=?")
+                    .authoritiesByUsernameQuery(
+                            "select username, role from user_roles where username=?")
+                    .and()
+                    .inMemoryAuthentication()
+                    .withUser("uname@gmail.com").password("pass").roles("USER").and()
+                    .withUser("admin@gmail.com").password("pass").roles("ADMIN").and()
+                    .withUser("expert@gmail.com").password("pass").roles("EXPERT");
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {//z pakietu krypto cos tam bo bylo wiele opcji
+            PasswordEncoder encoder = new BCryptPasswordEncoder();
+            return encoder;
+        }
+
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.httpBasic().and().authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/","/products.html").permitAll().anyRequest()
+                    .authenticated().and().csrf()
+                    .csrfTokenRepository(csrfTokenRepository()).and()
+                    .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+        }
+
+        private Filter csrfHeaderFilter() {
+            return new OncePerRequestFilter() {
+                @Override
+                protected void doFilterInternal(HttpServletRequest request,
+                                                HttpServletResponse response, FilterChain filterChain)
+                        throws ServletException, IOException {
+                    CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+                            .getName());
+                    if (csrf != null) {
+                        Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                        String token = csrf.getToken();
+                        if (cookie == null || token != null
+                                && !token.equals(cookie.getValue())) {
+                            cookie = new Cookie("XSRF-TOKEN", token);
+                            cookie.setPath("/");
+                            response.addCookie(cookie);
+                        }
+                    }
+                    filterChain.doFilter(request, response);
+                }
+            };
+        }
+
+        private CsrfTokenRepository csrfTokenRepository() {
+            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+            repository.setHeaderName("X-XSRF-TOKEN");
+            return repository;
+        }
     }
 }
---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,109 @@
+angular.module('hello', ['ngRoute']).config(function ($routeProvider, $httpProvider) {
+    //wstrzknelimsy ngroute do modulu
+
+    //tu sie ustawia kontoler i szablon html w zaleznosci od mapowania
+    $routeProvider.when('/', {
+        //index to slash / wiec jest Login to see your greeting
+        templateUrl: 'home.html',
+        controller: 'home'
+    }).when('/login', {
+        templateUrl: 'login.html',
+        controller: 'navigation'//nawigation tez jest na indeksie
+    }).when('/products', {
+        templateUrl: 'products.html',
+        controller: 'productsController'
+    }).when('/newAccount', {
+        templateUrl: 'newAccount.html',
+        controller: 'newAccountController'
+    }).otherwise('/');
+
+
+    //<wylacza okno dialogowe popup przegladarki pytajace o haslo
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+}).controller(
+    'navigation',
+
+    function ($rootScope, $scope, $http, $location, $route) {
+
+        $scope.tab = function (route) {
+            return $route.current && route === $route.current.controller;
+        };
+
+        var authenticate = function (credentials, callback) {
+            //tu buduje sie jakies zapytanie
+            var headers = credentials ? {
+                authorization: "Basic "
+                + btoa(credentials.username + ":"
+                + credentials.password)
+            } : {};
+
+            $http.get('user', {headers: headers}).success(function (data) {
+                //mapowanie user zwraca Principal a on ma getName()
+                if (data.name) {
+                    $rootScope.authenticated = true;
+                    $rootScope.loggedUser = credentials.username;
+
+                } else {
+                    $rootScope.authenticated = false;
+                }
+                callback && callback($rootScope.authenticated);
+            }).error(function () {
+                $rootScope.authenticated = false;
+                callback && callback(false);
+            });
+
+        }
+
+        //proboje samo bez wysylania fomularza jesli juz mamy to ciastko a jedynie odswiezylismy
+        authenticate();
+
+        $scope.credentials = {};
+        $scope.login = function () {
+            authenticate($scope.credentials, function (authenticated) {
+                if (authenticated) {
+                    console.log("Login succeeded")
+                    $location.path("/");
+                    $scope.error = false;
+                    $rootScope.authenticated = true;
+                } else {
+                    console.log("Login failed")
+                    $location.path("/login");
+                    $scope.error = true;
+                    $rootScope.authenticated = false;
+                }
+            })
+        };
+
+        $scope.logout = function () {
+            $http.post('logout', {}).success(function () {
+                $rootScope.authenticated = false;
+                $location.path("/");
+            }).error(function (data) {
+                console.log("Logout failed")
+                $rootScope.authenticated = false;
+            });
+        }
+
+    }).controller('home', function ($scope, $http) {
+        $http.get('/getResources/').success(function (data) {
+
+            $scope.greeting = data;
+
+
+        })
+    }) .controller('newAccountController', function ($scope, $http) {
+            $http.post('/receiveNewConfiguration', configuration).success(function (data) {
+                $scope.products = data;
+                $rootScope.errorNewAccount = false;
+            }).error(function (data) {
+                console.log("Setting up account failed")
+                $rootScope.errorNewAccount = true;
+            });
+
+    }).controller('productsController', function ($scope, $http) {
+        $http.get('/getProducts/').success(function (data) {
+            $scope.products = data;
+        });
+    }
+);
